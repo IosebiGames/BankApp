a746a434b4247efb826ad3a39d577db8f59ce067
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-06-26 11:11:29 +0400

    Changing Code

--- src/main/Executor.java
+++ src/main/Executor.java
@@ -1,8 +1,6 @@
 package main;
 
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 import java.awt.Color;
 import com.formdev.flatlaf.FlatDarkLaf;
 import mechanics.*;
@@ -13,11 +11,7 @@ public class Executor {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
 			  if(!new File("data.bap").exists()) {
-				  try {
-					UIManager.setLookAndFeel(new FlatDarkLaf());
-				  } catch (UnsupportedLookAndFeelException e) {
-					e.printStackTrace();
-				  }
+				  FlatDarkLaf.setup();
 				  new Screen("Banking App | Register", false, 644, 445, new Color(226, 76, 36), true);			 
 			  }else {
 				  CommonDataLoader.check();
